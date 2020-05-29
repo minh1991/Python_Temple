98d60a1cb156781fcf317920e56198669a49e691
minh1991 <hikari.mei.119@gmail.com>
2020-05-29 17:46:17 +0700

    20200529 -- views -html

--- ED_001/ED_001/settings.py
+++ ED_001/ED_001/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ED_001.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
